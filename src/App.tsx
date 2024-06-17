b82b903bb52a8a41b935eac87099fe219555499b
aman <amanpreetsinghjhiwant@outlook.com>
2024-06-17 21:04:27 +0530

    Created Roll Dice

---
+++ src/App.tsx
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import type {PropsWithChildren} from 'react';
+import ReactNativeHapticFeedback from "react-native-haptic-feedback";
+import {
+  ImageSourcePropType,
+  StyleSheet,
+  Text,
+  Image,
+  View,
+  Pressable,
+} from 'react-native';
+
+
+import DiceOne from '../assets/One.png'
+import DiceTwo from '../assets/Two.png'
+import DiceThree from '../assets/Three.png'
+import DiceFour from '../assets/Four.png'
+import DiceFive from '../assets/Five.png'
+import DiceSix from '../assets/Six.png'
+
+type diceProps = PropsWithChildren<{
+  imageUrl :ImageSourcePropType
+}>
+
+const options = {
+  enableVibrateFallback: true,
+  ignoreAndroidSystemSettings: false,
+};
+
+const Dice = ({imageUrl}:diceProps):JSX.Element => {
+  return (
+    <View>
+      <Image style={styles.diceImage} source={imageUrl}/>
+    </View>
+  )
+}
+
+function App(): React.JSX.Element {
+  const [diceImage, setDiceImage] = useState<ImageSourcePropType>(DiceOne)
+
+  const rollDiceOnTap = ()=>{
+    let randomNumber = Math.floor(Math.random()*6)+1;
+    switch(randomNumber){
+      case 1:
+        setDiceImage(DiceOne);
+        break;
+      case 2:
+          setDiceImage(DiceTwo);
+          break;
+      case 3:
+        setDiceImage(DiceThree);
+        break;
+      case 4:
+        setDiceImage(DiceFour);
+        break;
+      case 5:
+        setDiceImage(DiceFive);
+        break;
+      case 6:
+        setDiceImage(DiceSix);
+        break;
+      default:
+        setDiceImage(DiceOne);
+        break;
+    }
+    ReactNativeHapticFeedback.trigger("impactLight", options);
+  }
+  return (
+    <View style={styles.container}>
+      <Dice imageUrl={diceImage}/>
+      <Pressable onPress={rollDiceOnTap}>
+        <Text style={styles.rollDiceBtnText}>Roll the Dice</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFF2F2',
+  },
+  diceContainer: {
+    margin: 12,
+  },
+  diceImage: {
+    width: 200,
+    height: 200,
+  },
+  rollDiceBtnText: {
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderWidth: 2,
+    borderRadius: 8,
+    borderColor: '#E5E0FF',
+    fontSize: 16,
+    color: '#8EA7E9',
+    fontWeight: '700',
+    textTransform: 'uppercase',
+  },
+});
+
+export default App;
